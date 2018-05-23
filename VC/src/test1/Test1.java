e9a538867ed2eaffe47056a2d3aec8e8e1b765b9
pratap393 <39005741+pratap393@users.noreply.github.com>
2018-05-23 14:42:52 +0530

    hii

--- VC/src/test1/Test1.java
+++ VC/src/test1/Test1.java
@@ -1,10 +1,26 @@
 package test1;
 
-public class Test1 {
+class ABC extends Thread{
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("hiiiiii");
+			}
+	}
+}
+public class Test1{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
 		System.out.println("Hiiii1Pull");
+		ABC t=new ABC();
+		t.setName("Ram1");
+		t.setPriority(10);
+		t.start();
+		for(int i=0;i<10;i++) {
+			System.out.println("byeeeere");
+			}
+		
 	}
+	
 
 }
